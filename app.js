268ca7c4260a533d00093b0c190b4b4c85d735f5
Shabash Khan <95525622+shahbazDev@users.noreply.github.com>
2022-08-28 22:06:00 +0530

    refactor:ex02 done

---
+++ app.js
@@ -0,0 +1,34 @@
+const dateOfBirth = document.querySelector("#date-of-birth");
+const luckyNumber = document.querySelector("#lucky-number");
+const checkButton = document.querySelector("#check-number");
+const outputBox = document.querySelector("#output-Box");
+
+function compareValues(sum,luckyNumber){
+    if(sum%luckyNumber===0){
+        outputBox.innerText ="your birthday is lucky😍"
+    }
+    else{
+        outputBox.innerText ="your birthday is not lucky😢"
+    }
+
+}
+
+function checkBirthDateIsLucky(){
+ const dob = dateOfBirth.value;
+ const sum=calculateSum(dob);
+ compareValues(sum,luckyNumber.value)
+}
+
+function calculateSum(dob){
+    dob=dob.replaceAll("-",""); 
+  let sum=0;
+    for(i=0;i<dob.length;i++){
+        sum=sum + Number(dob.charAt(i));
+    }
+    return sum;
+}
+
+
+
+checkButton.addEventListener('click',checkBirthDateIsLucky)
+
